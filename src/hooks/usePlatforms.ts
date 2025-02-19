6ce9deb7895859f328d2840e0ecc00b6f479020f
nainsworth <nja8161995@gmail.com>
2025-02-18 21:02:49 -0600

    Refactor: Simplified to lookup hooks

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
@@ -13,6 +14,8 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
+    staleTime: 24 * 60 * 60 * 100, //24 Hours
+    initialData: platforms,
   });
 
 export default usePlatforms;
