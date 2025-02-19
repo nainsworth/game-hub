829a39776b63d4bfbb7152f4a470564c31d35a0e
nainsworth <nja8161995@gmail.com>
2025-02-18 20:46:08 -0600

    Refactor: Simplified Game Query

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,5 +1,3 @@
-import useGenres, { Genre } from "../hooks/useGenre";
-import getCroppedImageUrl from "../services/image-url";
 import {
   Button,
   Heading,
@@ -9,13 +7,15 @@ import {
   ListItem,
   Spinner,
 } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenre";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
-  selectedGenre: Genre | null;
+  selectedGenreId?: number;
 }
 
-const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenreId }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -41,7 +41,7 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
                 whiteSpace="normal"
                 variant="link"
                 fontSize="lg"
-                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                fontWeight={genre.id === selectedGenreId ? "bold" : "normal"}
                 onClick={() => onSelectGenre(genre)}
               >
                 {genre.name}
