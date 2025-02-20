bd1eb71565043c66defb308835ea4b728c10831b
nainsworth <nja8161995@gmail.com>
2025-02-19 21:22:41 -0600

    Added coming soon text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -13,7 +13,8 @@ const GameDetailPage = () => {
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      {/* <Text>{game.description_raw}</Text> */}
+      <Text>Coming Soon!</Text>
     </>
   );
 };
